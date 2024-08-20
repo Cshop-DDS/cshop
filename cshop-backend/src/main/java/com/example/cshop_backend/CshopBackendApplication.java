484470a77f922125768555dbe49b73ba2284ed62
shinhy <94shinshinhy@gmail.com>
2024-08-20 15:23:47 +0900

    backend commit

---
+++ cshop-backend/src/main/java/com/example/cshop_backend/CshopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.cshop_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CshopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CshopBackendApplication.class, args);
+	}
+
+}
---
+++ cshop-backend/src/test/java/com/example/cshop_backend/CshopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cshop_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CshopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
